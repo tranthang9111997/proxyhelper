3658ed19b1bab4822b3d2886a151d2b441da3454
Nguyễn Trần Thắng <51274694+tranthang9111997@users.noreply.github.com>
2022-04-24 06:12:38 +0700

    pc

---
+++ model/proxy.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+let Proxy = new Schema(
+  {
+    username: {
+      type: String
+    },
+    password: {
+      type: String
+    },
+    host: {
+      type: String
+        },
+    port: {
+      type: Number
+    },
+    numberOfConnection:{    
+      type: Number,
+      default: 0
+    },
+    type:{
+      type: String,
+      default: "none"
+    },
+    fowardPort:{
+      type: Number,
+      default: 0
+    },
+    data:{
+      type:String
+    },
+    time:{
+      type:Number,
+      default:0
+    }
+}
+);
+
+module.exports = mongoose.model("Proxy", Proxy);
